6f9e911e993605e3d90e013a21423d5cd7c2f6aa
Marc <marc.vornetran@gmail.com>
2015-04-23 16:04:07 +0200

    Override getCharge() in ChildrensPrice

--- src/ChildrensPrice.java
+++ src/ChildrensPrice.java
@@ -4,4 +4,14 @@ class ChildrensPrice extends Price {
         return Movie.CHILDRENS;
     }
 
+    double getCharge(int daysRented){
+        double result = 1.5;
+
+        if(daysRented > 3){
+            result += (daysRented - 3) * 1.5;
+        }
+
+        return result;
+    }
+
 }
